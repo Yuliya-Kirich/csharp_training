96969e8b22f1fd8188a7ff4041841c751b923462
Yuliya_Kirich <yvkirich@gmail.com>
2018-09-11 20:59:45 +0300

    Проверка после модификации группы, что изменился тот именно элемент, что был указан в условии

--- addressbook-web-tests/addressbook-web-tests/tests/GroupModificationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupModificationTests.cs
@@ -23,6 +23,7 @@ namespace WebAddressbookTests
 
 
             List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData oldData = oldGroups[0]; // информация для проверки запоминается заранее
 
             app.Groups.Modify(0, newData);
 
@@ -33,6 +34,19 @@ namespace WebAddressbookTests
             oldGroups.Sort();
             newGroups.Sort();
             Assert.AreEqual(oldGroups, newGroups);
+            //Нужно проверить, что изменился тот именно элемент, что был указан в условии
+            // т.е. идентификатор элемента находим и проверяем, что его имя стало таким, каким должно было стать
+            foreach(GroupData group in newGroups)
+            {
+                if(group.Id == oldData.Id)
+                {
+                    Assert.AreEqual(newData.Name, group.Name);
+                }
+            }
+            //Все, что будем использовать в проверках, нужно сохранить заранее, чтобы безопасно использовать эти данные в проверках
+            //Элемент с нулевым идентификатором, это тот который был до сортировки, и именно он должен меняться
+            //т.к. мы пересортировали список, 1 элемент взять нельзя. Поэтому вся информация для теста должна быть заполнена заранее.
         }
+
     }
 }
